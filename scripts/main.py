b5292f5a6b6d2621e1970e7243113c3f4ebba958
Milan Lustig <mguinangreen726@gmail.com>
2023-04-16 15:45:36 -0400

    more python

--- scripts/main.py
+++ scripts/main.py
@@ -2,5 +2,12 @@ import pandas as pd
 
 data = pd.read_excel('data.xlsx')
 
-print(data)
+dictionaryOfData = {}
 
+for num in data['teamNumber']:
+    if num not in dictionaryOfData:
+        dictionaryOfData[num] = [data[data['teamNumber'] == num]]
+    else:
+        dictionaryOfData[num].append(data[data['teamNumber'] == num])
+
+for key in dictionaryOfData:
